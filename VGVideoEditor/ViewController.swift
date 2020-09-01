5a5f90adcf5d41de321419091af6e561fd97151c
周智伟 <zhouzhiwei@laihua.com>
2020-09-01 13:45:01 +0800

    Initial Commit

---
+++ VGVideoEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VGVideoEditor
+//
+//  Created by 周智伟 on 2020/9/1.
+//  Copyright © 2020 vege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
